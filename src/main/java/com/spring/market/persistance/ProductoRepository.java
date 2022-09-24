92d3421dbadcc6a81792b2e3347f2554ef6acb3c
Johjan Puerta <johjan_castano98@hotmail.com>
2022-09-24 00:00:00 -0500

    Uso de @Repository

--- src/main/java/com/spring/market/persistance/ProductoRepository.java
+++ src/main/java/com/spring/market/persistance/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.spring.market.persistance;
 
 import com.spring.market.persistance.crud.ProductoCrudRepository;
 import com.spring.market.persistance.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository //le indicamos a la clase que es la encargada de interactuar con la base de datos.
 public class ProductoRepository {
   private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +21,17 @@ public class ProductoRepository {
 
   public Optional<List<Producto>> getEscasos(int cantidad){
     return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+  }
+
+  public Optional<Producto> getProducto(int idProducto){
+    return productoCrudRepository.findById(idProducto);
+  }
+
+  public Producto save(Producto producto){
+    return productoCrudRepository.save(producto);
+  }
+
+  public void delete(int idProducto){
+    productoCrudRepository.deleteById(idProducto);
   }
 }

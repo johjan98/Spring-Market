f7a5dd5c7801d4aabd4ca1b53ffffeaca5eef606
Johjan Puerta <johjan_castano98@hotmail.com>
2022-09-25 01:38:54 -0500

    Implementacion completa de un nuevo servicio en la API: Purchases

--- src/main/java/com/spring/market/persistance/entity/Compra.java
+++ src/main/java/com/spring/market/persistance/entity/Compra.java
@@ -1,5 +1,6 @@
 package com.spring.market.persistance.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -31,8 +32,8 @@ public class Compra {
   @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
   private Cliente cliente;
 
-  @OneToMany(mappedBy = "compra")
-  private List<ComprasProducto> comprasProductos;
+  @OneToMany(mappedBy = "compra", cascade = {CascadeType.ALL})
+  private List<ComprasProducto> productos;
 
   public Integer getIdCompra() {
     return idCompra;
@@ -80,5 +81,21 @@ public class Compra {
 
   public void setEstado(String estado) {
     this.estado = estado;
+  }
+
+  public Cliente getCliente() {
+    return cliente;
+  }
+
+  public void setCliente(Cliente cliente) {
+    this.cliente = cliente;
+  }
+
+  public List<ComprasProducto> getProductos() {
+    return productos;
+  }
+
+  public void setProductos(List<ComprasProducto> productos) {
+    this.productos = productos;
   }
 }
---
+++ src/main/java/com/spring/market/persistance/mapper/PurchaseItemMapper.java
@@ -0,0 +1,27 @@
+package com.spring.market.persistance.mapper;
+
+import com.spring.market.domain.PurchaseItem;
+import com.spring.market.persistance.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
+public interface PurchaseItemMapper {
+  @Mappings({
+          @Mapping(source = "id.idProducto", target = "productId"),
+          @Mapping(source = "cantidad", target = "quantity"),
+          //@Mapping(source = "total", target = "total"),  //No es necesario ponerlo, tienen el mismo nombre.
+          @Mapping(source = "estado", target = "active")
+  })
+  PurchaseItem toPurchaseItem(ComprasProducto comprasProducto);
+
+  @InheritInverseConfiguration
+  @Mappings({
+          @Mapping(target = "compra", ignore = true),
+          @Mapping(target = "producto", ignore = true),
+          @Mapping(target = "id.idCompra", ignore = true)
+  })
+  ComprasProducto toComprasProducto(PurchaseItem purchaseItem);
+}
